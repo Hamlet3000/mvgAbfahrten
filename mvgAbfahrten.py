e01a04fa61827b9abf4dfd685d67b8134e699fcc
Hamlet3000 <75612866+Hamlet3000@users.noreply.github.com>
2023-12-03 11:17:56 +0100

    Add files via upload

--- mvgAbfahrten.py
+++ mvgAbfahrten.py
@@ -14,30 +14,45 @@
 #############################################################################
 
 import json
+import time
 import datetime
+from datetime import datetime
 import paho.mqtt.client as mqtt
 from mvg import MvgApi
 
-def get_departures(station_name):
+def get_departures(station_name, destination_list):
     station_id = MvgApi.station(station_name)
 
     if station_id:
         mvg_api = MvgApi(station_id['id'])
-        departures = mvg_api.departures(limit=5, offset=10)
+        departures = mvg_api.departures(limit=20, offset=10)
 
         departure_data = {}
 
-        for idx, item in enumerate(departures):
-            time_readable = datetime.datetime.fromtimestamp(item.get('time')).strftime('%H:%M Uhr')
-            info = f"{time_readable} {item['type']} {item['line']} von {station_name} -> {item['destination']} - CANCELLED" if item['cancelled'] else f"{time_readable} {item['type']} {item['line']} von {station_name} -> {item['destination']}"
-            item['info'] = info
-            departure_data[f"Abfahrt{idx}"] = item
+        idx = 0
+        for item in departures:
+            if item['destination'] in destination_list:
+                time_readable = datetime.fromtimestamp(item.get('time')).strftime('%H:%M Uhr')
+                akt_time = int(time.mktime(datetime.now().timetuple()))
+                dif_in_min = int((item['time'] - akt_time) / 60)
+
+                if item['cancelled']:
+                    info = f"in {dif_in_min} Min. ({time_readable}) {item['type']} {item['line']} von {station_name} -> {item['destination']} - CANCELLED" 
+                else:
+                    info = f"in {dif_in_min} Min. ({time_readable}) {item['type']} {item['line']} von {station_name} -> {item['destination']}" 
+
+                item['info'] = info
+                departure_data[f"Abfahrt{idx}"] = item
+
+                #print(item['info'])
+                idx = idx+1
 
         return departure_data
     else:
         return None
 
 def publish_mqtt(data, subtopic):
+
 
     subtopic = subtopic.replace(" ","_")
 
@@ -62,9 +77,10 @@ def publish_mqtt(data, subtopic):
 
 def main():
     station_list = ['Taimerhofstraße', 'Johanneskirchner Straße']
+    destination_list = ['Effnerplatz', 'Nordbad', 'Max-Weber-Platz', 'Romanplatz']
 
     for station_name in station_list:
-        departure_data = get_departures(station_name)
+        departure_data = get_departures(station_name, destination_list)
         if departure_data:
             publish_mqtt(departure_data, station_name)
 
